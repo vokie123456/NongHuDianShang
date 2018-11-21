c4c3ccd8ca853db65620d5c458a2727dd5a0bdcf
kangshan <kangshan_dz@sm.vvip-u.com>
2018-11-21 15:41:24 +0800

    update some code

--- src/com/woniuxy/farmerec/controller/UserController.java
+++ src/com/woniuxy/farmerec/controller/UserController.java
@@ -2,10 +2,10 @@ package com.woniuxy.farmerec.controller;
 
 public class UserController {
 
-		public static void main(String[] args) {
-			System.out.println("hsjsdkdfzvkff");
+	public static void main(String[] args) {
+		System.out.println("hsjsdkdfzvkff");
 
-			System.out.println("1231312");
-		}
+		System.out.println("1231312");
+	}
 
 }
